e6a95522f744e46a77c63b71659d53293a48aa98
Randy Hollaway <33422557+RSHollaway@users.noreply.github.com>
2021-01-11 08:02:32 -0500

    Create ifWorking.ps1

---
+++ ifWorking.ps1
@@ -0,0 +1,12 @@
+if ($Working) {
+    Headphones = true;
+    Focus = 100;
+    Conversation = null;
+}
+
+while (!Dead) {
+    Eat (Tacos);
+    Sleep (Never);
+    Automate (PowerShell);
+    Play (IceHockey);
+}
